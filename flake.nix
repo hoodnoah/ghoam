6c9b7e6ee12ff15eddc3dba5d7ba567078e449fe
Noah Hood <hood.noah@icloud.com>
2025-04-17 19:19:11 -0400

    move services to their own directories, set up template dune/OCaml hello-world

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Go 1.24 development environment";
+  description = "A Nix-flake-based Go 1.24 + OCAML development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
@@ -23,22 +23,56 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
+            # - Go toolchain -
             # go (version is specified by overlay)
             go
-
-            # goimports, godoc, etc.
             gotools
-
-            # https://github.com/golangci/golangci-lint
             golangci-lint
+            gopls
+            gomodifytags
+            gotests
+            gore
+            godef
 
-	    gopls
+            # - OCaml toolchain -
+            ocaml  # OCaml compiler
+            opam # OPAM package manager
+            dune_2 # Dune build system
 
-	    gomodifytags
-	    gotests
-	    gore
-	    godef
+            # - Supporting tools - 
+            protobuf # protoc compiler
+
+            # - C-level libraries for OCaml gRPC & Protobuf plugins - 
+            pkg-config
+            grpc
+            protobufc
+            libffi
+            zlib
+            openssl
           ];
+
+          shellHook = ''
+            # 1) Bootstrap OPAM non-interactively
+            if ! opam root >/dev/null 2>&1; then
+              opam init --bare --disable-sandboxing --no-setup -yes
+            fi
+
+            # 2) Create an "empty" local switch that picks up Nix' `ocaml`
+            if [ ! -d .opam-switch ]; then
+              opam switch create . --empty --yes
+            fi
+
+            # 3) Load OPAM environment
+            eval #(opam env)
+
+            # 4) Install OCaml dependencies if they're missing
+            if ! ocaml list --installed | grep -q grpc-lwc; then
+              opam install --yes yojson lwt grpc-lwt grpc ocaml-protoc-plugin
+            fi
+
+            # 5) Make sure ocaml-protoc-plugin (and any other OPAM tools) are on PATH
+            export PATH=$PATH:$(opam var bin)
+          '';
         };
       });
     };
